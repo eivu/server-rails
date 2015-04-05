e0433e254057d8c5a88337cebc3bfd732f5f6095
Robert Jenkins <bobert@gmail.com>
2015-04-05 01:11:37 -0400

    - created relation between cloud_file and folder

--- app/models/cloud_file.rb
+++ app/models/cloud_file.rb
@@ -1,13 +1,15 @@
 class CloudFile < ActiveRecord::Base
   mount_uploader :asset, CloudFileUploader
 
+  belongs_to :folder
+
   validates_uniqueness_of :md5
 
 
   def self.upload!(path_to_file)
     # md5 = Digest::MD5.hexdigest(File.read('path_to_file'))
     ActiveRecord::Base.transaction do
-      cloud_file = CloudFile.new
+      cloud_file = CloudFile.new :folder => Folder.create_from_path(path_to_file)
       File.open(path_to_file) do |file|
         cloud_file.asset = file
       end
