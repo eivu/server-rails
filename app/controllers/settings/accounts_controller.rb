f0e2dfbef43c3392532013bc095f1b60c8fd2513
Robert Jenkins <bobert@gmail.com>
2015-05-24 23:34:53 -0400

    - updating accounts work on settings#account

--- app/controllers/settings/accounts_controller.rb
+++ app/controllers/settings/accounts_controller.rb
@@ -1,2 +1,17 @@
 class Settings::AccountsController < ApplicationController
+
+  def update
+    begin
+      #if password stuff is blank that means we are not trying to reset the password, so remove the keys from params
+      if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
+        params[:user].delete(:password)
+        params[:user].delete(:password_confirmation)
+      end
+      current_user.update_attributes! params[:user].permit(:password, :password_confirmation, :access_key_id, :secret_access_key)
+      flash[:success] = "Account has been updated"
+    rescue ActiveRecord::RecordInvalid => error
+      flash[:notice] = error.message
+    end
+    redirect_to settings_account_path
+  end
 end
