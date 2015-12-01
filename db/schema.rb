3ab81837c8d534112e7188b393f9bd8d417c4fa0
Robert Jenkins <bobert@gmail.com>
2015-12-01 02:44:27 -0800

    - added profile

--- db/schema.rb
+++ db/schema.rb
@@ -67,12 +67,6 @@ ActiveRecord::Schema.define(version: 20150530210203) do
     t.datetime "updated_at"
   end
 
-  create_table "tags", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "users", force: true do |t|
     t.string   "username"
     t.datetime "created_at"
