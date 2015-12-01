a59e05be71c699bf54208087035ea0e6c2a31504
Robert Jenkins <bobert@gmail.com>
2015-12-01 02:07:08 -0800

    - app now uses env variables so it works with heroku

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,1 +1,8 @@
-AWS_CONFIG = YAML.load_file(Rails.root + 'config/aws.yml')[Rails.env].with_indifferent_access
+# AWS_CONFIG = YAML.load_file(Rails.root + 'config/aws.yml')[Rails.env].with_indifferent_access
+AWS_CONFIG = 
+  { 
+    :region => 'us-east-1',
+    :access_key_id => ENV["EIVU_AWS_ACCESS_KEY_ID"],
+    :secret_access_key => ENV["EIVU_AWS_SECRET_ACCESS_KEY"],
+    :bucket => 'eivutest'
+  }
