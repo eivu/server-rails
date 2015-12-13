16fe540523ec638b696ca34e86bed4e13d922522
Robert Jenkins <bobert@gmail.com>
2015-12-13 14:50:16 -0500

    - added has_token to gemfile - added token column to user model

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'mimemagic', '~> 0.3.0' #determine mime type by magic
 gem 'mini_magick', '3.5' #there's an issues with v3.7 https://github.com/carrierwaveuploader/carrierwave/issues/1282
 gem 'rest-client', '~> 1.8.0' #A simple HTTP and REST client for Ruby, inspired by the Sinatra microframework style of specifying actions
 gem 'attr_encrypted', '~> 1.3.3' #Generates attr_accessors that encrypt and decrypt attributes transparently
+gem 'has_secure_token' #generate the 24-character unique token
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer',  platforms: :ruby
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150530210203) do
+ActiveRecord::Schema.define(version: 20151213194735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 20150530210203) do
     t.string   "encrypted_secret_access_key"
     t.string   "encrypted_secret_access_key_salt"
     t.string   "encrypted_secret_access_key_iv"
+    t.string   "token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
