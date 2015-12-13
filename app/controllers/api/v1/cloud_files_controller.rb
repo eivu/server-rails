1a5cdf3cbc00763901ed80de73bcea9339788da0
Robert Jenkins <bobert@gmail.com>
2015-12-13 16:13:25 -0500

    - endpoint /api/v1/cloud_files/{md5}/authorize now works

--- app/controllers/api/v1/cloud_files_controller.rb
+++ app/controllers/api/v1/cloud_files_controller.rb
@@ -2,7 +2,11 @@ class Api::V1::CloudFilesController < Api::V1Controller
 
 
   def authorize
-    # binding.pry
+    if current_user.cloud_files.where(:md5 => params[:id]).blank?
+      render :json => { :status => :ok, :message => "proceed with upload" }
+    else
+      render :json => { :status => :error, :message => "file already exists w/ md5 #{params[:id]}" }, :status => :unauthorized
+    end
   end
 
 end
