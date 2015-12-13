49f1b79e4fe3777525c32646e2683f863d07f6b6
Robert Jenkins <bobert@gmail.com>
2015-12-13 16:47:58 -0500

    - changed the responses of /api/cloud_files#authorize

--- app/controllers/api/v1/cloud_files_controller.rb
+++ app/controllers/api/v1/cloud_files_controller.rb
@@ -1,11 +1,10 @@
 class Api::V1::CloudFilesController < Api::V1Controller
 
-
   def authorize
     if current_user.cloud_files.where(:md5 => params[:id]).blank?
-      render :json => { :status => :ok, :message => "proceed with upload" }
+      render :json => { :status => 200, :status_msg => :ok, :message => "proceed with upload" }
     else
-      render :json => { :status => :error, :message => "file already exists w/ md5 #{params[:id]}" }, :status => :unauthorized
+      render :json => { :status_msg => :error, :message => "file already exists w/ md5 #{params[:id]}" }, :status => :unauthorized
     end
   end
 
