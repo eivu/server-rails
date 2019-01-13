2a31200a0a7a9da238038fea7c4890e1db62da2d
Robert Jenkins <bobert@gmail.com>
2019-01-13 14:23:34 -0500

    Fingerprinter#parse_artists is done

--- app/models/fingerprinter.rb
+++ app/models/fingerprinter.rb
@@ -44,5 +44,33 @@ class Fingerprinter
     end
   end
 
+
+  def test_data
+    [
+      [{"joinphrase"=>" & ", "id"=>"f82bcf78-5b69-4622-a5ef-73800768d9ac", "name"=>"JAY Z"}, {"id"=>"164f0d73-1234-4e2c-8743-d77bf2191051", "name"=>"Kanye West"}],
+      [{"joinphrase"=>" & ", "id"=>"0d7b68b7-6039-4883-8214-9eeb13f8c283", "name"=>"Zacari"}, {"id"=>"d8b9e0dd-2d22-49f3-9b19-6440527f1c8b", "name"=>"Babes Wodumo"}],
+      [{"id"=>"381086ea-f511-4aba-bdf9-71c753dc5077", "name"=>"Kendrick Lamar"}],
+      [{"joinphrase"=>" & ", "id"=>"381086ea-f511-4aba-bdf9-71c753dc5077", "name"=>"Kendrick Lamar"}, {"id"=>"272989c8-5535-492d-a25c-9f58803e027f", "name"=>"SZA"}],
+      [{"joinphrase"=>" & ", "id"=>"f82bcf78-5b69-4622-a5ef-73800768d9ac", "name"=>"JAY Z"},
+       {"joinphrase"=>" feat. ", "id"=>"164f0d73-1234-4e2c-8743-d77bf2191051", "name"=>"Kanye West"},
+       {"id"=>"859d0860-d480-4efd-970c-c05d5f1776b8", "name"=>"Beyoncé"}]
+    ]
+  end
+
+  def parse_artists(array)
+    attributes = { :primary => [], :secondary => [] }
+    array = array.collect(&:symbolize_keys)
+    # & means primary, anything else means supporting
+    join_chars = ["&"] + array.collect {|x| x[:joinphrase]}.compact.collect(&:strip)
+    array.each_with_index do |hash, index|
+      if join_chars[index] == "&"
+        attributes[:primary] << hash.slice(:id, :name)
+      else
+        attributes[:secondary] << hash.slice(:id, :name)
+      end
+    end
+    attributes
+  end
+
 end
 
