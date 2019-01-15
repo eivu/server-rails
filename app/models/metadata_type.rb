69fd8290340a5a08d60ff667ff46562e4f538d6e
Robert Jenkins <bobert@gmail.com>
2019-01-14 20:32:01 -0500

    removed validated_uniques for user_id. user_id is not in the table

--- app/models/metadata_type.rb
+++ app/models/metadata_type.rb
@@ -1,4 +1,7 @@
 class MetadataType < ActiveRecord::Base
   has_many :metadata
-  validates_uniqueness_of :value, :scope => :user_id
+  # validates_uniqueness_of :value, :scope => :user_id
+
+  # @@ACOUSTID_FINGERPRINT = MetadataType.find_or_create_by!(:value => "AcoustID Fingerprint")
+  # ACOUSTID_FINGERPRINT = @@ACOUSTID_FINGERPRINT
 end
