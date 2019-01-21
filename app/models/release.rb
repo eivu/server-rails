9ad1cb1ac8c48a7d7a04769756d647517c80504e
Robert Jenkins <bobert@gmail.com>
2019-01-21 00:50:45 -0500

    added depenedent destroy for relase has_many cloud_files

--- app/models/release.rb
+++ app/models/release.rb
@@ -2,7 +2,7 @@ class Release < ApplicationRecord
   include Determinable
   belongs_to :release_type
 
-  has_many :cloud_files, :counter_cache => :cloud_files_count
+  has_many :cloud_files, :dependent => :destroy, :counter_cache => :cloud_files_count
   has_many :artist_releases, :dependent => :destroy
   has_many :artists, :through => :artist_releases
 
