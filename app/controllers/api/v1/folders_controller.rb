d2cea1d139fd583d3258b924c98c4a904e370346
Robert Jenkins <bobert@gmail.com>
2019-01-23 20:35:09 -0500

    Folders action is now index and not show. added /api/v1/folders to route

--- app/controllers/api/v1/folders_controller.rb
+++ app/controllers/api/v1/folders_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::FoldersController < Api::V1Controller
 
-  def show
+  def index
     render :json => { :status => 200, :status_msg => :ok, :data => Folder.roots.collect{|f| f.subtree.arrange_serializable }}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
   namespace :api, :defaults => { :format => 'json' }  do
     namespace :v1 do
+      resources :folders
       resources :cloud_files do 
         member do
           post :authorize
