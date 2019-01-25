7163804d78f51ae5a8d8aa0f43052f91955fdfa8
Robert Jenkins <bobert@gmail.com>
2019-01-24 21:01:50 -0500

    updated test data in eivu js pack

--- app/models/tree_serializer.rb
+++ app/models/tree_serializer.rb
@@ -7,12 +7,12 @@ class TreeSerializer
     when "CloudFile"
       tree = object_attr.merge("entry_type" => "file")
     when "Folder"
-      sub_folders = object.children.collect {|sub_folder| TreeSerializer.build(sub_folder)}
-      cloud_files = object.children.collect {|cloud_file| TreeSerializer.build(cloud_file)}
+      sub_folders = object.children.order(:name).collect {|sub_folder| TreeSerializer.build(sub_folder)}
+      cloud_files = object.children.order(:name).collect {|cloud_file| TreeSerializer.build(cloud_file)}
       tree = object_attr.merge("entry_type" => "grouping", "klass" => object.class.name, "children" => (sub_folders + cloud_files))
     when "NilClass"
       folders = Folder.roots.collect {|folder| TreeSerializer.build(folder)}
-      cloud_files = CloudFile.where(:folder_id => nil).collect {|cloud_file| TreeSerializer.build(cloud_file)}
+      cloud_files = CloudFile.where(:folder_id => nil).order(:name).collect {|cloud_file| TreeSerializer.build(cloud_file)}
       tree = folders + cloud_files
     else
       raise "Unkown case for TreeSerializer#initialize with #{object.class}"
