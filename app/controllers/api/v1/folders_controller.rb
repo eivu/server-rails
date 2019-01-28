7aead545fba99615cbdc0dd0e0f47b9e4c60621a
Robert Jenkins <bobert@gmail.com>
2019-01-28 01:31:39 +0000

    /api/v1/folder#index now returns TreeSerializer.build(nil) and skips find_user

--- app/controllers/api/v1/folders_controller.rb
+++ app/controllers/api/v1/folders_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::FoldersController < Api::V1Controller
+	skip_before_action :find_user
 
   def index
-    render :json => { :status => 200, :status_msg => :ok, :data => Folder.roots.collect{|f| f.subtree.arrange_serializable }}
+    render :json => { :status => 200, :status_msg => :ok, :data => TreeSerializer.build }
   end
 end
