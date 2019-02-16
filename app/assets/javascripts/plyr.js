e33ab9cd5000ec62f701139ff39c90992bf9c65f
Robert Jenkins <bobert@gmail.com>
2019-02-16 19:53:54 +0000

    plyr is working

--- app/assets/javascripts/plyr.js
+++ app/assets/javascripts/plyr.js
@@ -1,38 +1,51 @@
+
+
+
 document.addEventListener('DOMContentLoaded', function(event) {
-  // This is the bare minimum JavaScript. You can opt to pass no arguments to setup.
-  // const player = new Plyr('#plyr_container');
-  const player = new Plyr(document.getElementById('plyr_container'));
-  
-  // Expose
+  // Change "{}" to your options:
+  // https://github.com/sampotts/plyr/#options
+  const player = new Plyr('audio', {});
+
+  // Expose player so it can be used from the console
   window.player = player;
-
-  // // Bind event listener
-  // function on(selector, type, callback) {
-  //   document.querySelector(selector).addEventListener(type, callback, false);
-  // }
-
-  // // Play
-  // on('.js-play', 'click', () => { 
-  //   player.play();
-  // });
-
-  // // Pause
-  // on('.js-pause', 'click', () => { 
-  //   player.pause();
-  // });
-
-  // // Stop
-  // on('.js-stop', 'click', () => { 
-  //   player.stop();
-  // });
-
-  // // Rewind
-  // on('.js-rewind', 'click', () => { 
-  //   player.rewind();
-  // });
-
-  // // Forward
-  // on('.js-forward', 'click', () => { 
-  //   player.forward();
-  // });
 });
+
+
+// document.addEventListener('DOMContentLoaded', function(event) {
+//   // This is the bare minimum JavaScript. You can opt to pass no arguments to setup.
+//   // const player = new Plyr('#plyr_container');
+//   const player = new Plyr(document.getElementById('plyr_container'));
+  
+//   // Expose
+//   window.player = player;
+
+//   // // Bind event listener
+//   // function on(selector, type, callback) {
+//   //   document.querySelector(selector).addEventListener(type, callback, false);
+//   // }
+
+//   // // Play
+//   // on('.js-play', 'click', () => { 
+//   //   player.play();
+//   // });
+
+//   // // Pause
+//   // on('.js-pause', 'click', () => { 
+//   //   player.pause();
+//   // });
+
+//   // // Stop
+//   // on('.js-stop', 'click', () => { 
+//   //   player.stop();
+//   // });
+
+//   // // Rewind
+//   // on('.js-rewind', 'click', () => { 
+//   //   player.rewind();
+//   // });
+
+//   // // Forward
+//   // on('.js-forward', 'click', () => { 
+//   //   player.forward();
+//   // });
+// });
