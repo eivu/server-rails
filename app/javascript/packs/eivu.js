9faa9f5654d55b17888fcc63b3157ebcfd192542
Robert Jenkins <bobert@gmail.com>
2019-02-20 17:18:48 -0500

    move tree-node component into its own file

--- app/javascript/packs/eivu.js
+++ app/javascript/packs/eivu.js
@@ -3,7 +3,7 @@ import Vuex from 'vuex'
 import VuePlyr from 'vue-plyr'
 import axios from 'axios';
 import plyr from './components/plyr.vue'
-import CloudFile from './components/cloud-file.vue'
+import TreeNode from './components/tree-node.vue'
 import "babel-core/register"
 import 'babel-polyfill'
 
@@ -13,8 +13,6 @@ Vue.use(Vuex)
 
 Vue.prototype.$http = axios;
 // Vue.use(axios) doesn't work i don't know why
-
-
 
 
 
@@ -94,70 +92,11 @@ const store = new Vuex.Store({
 })
 
 
-
-Vue.component('tree-node', {
-  props: ['node'],
-  components: { CloudFile },
-  data() {
-    return { showChildren: false, dataLoaded: false, children: [] }
-  },
-  template: 
-    `<li v-bind:id="node.id" v-bind:class="node.entry_type">
-        <span v-if="node.entry_type == 'grouping'">
-          <div v-bind:class="node.klass" v-bind:type="node.entry_type" @click="toggleChildren">{{ node.name }}</div>
-        </span>
-        <span v-else-if="node.entry_type == 'file'">
-          <CloudFile v-bind:file="node"></CloudFile>
-        </span>
-        <span v-else>
-          <div>{{ node.name }}</div>
-        </span>
-       
-      <ul v-if="node.children && showChildren">
-        <tree-node v-for="child in children" v-bind:node="child" :key="child.vue_id">
-        </tree-node>
-      </ul>
-    </li>`,
-  computed: {
-    // testing: function() {
-    //   return  this.$store.getters.nextAutoTrackObject && this.$store.getters.nextAutoTrackObject.node.name;
-    // },
-  },
-  methods: {
-    test () {
-      this.$store.getters.nextAutoTrackObject;
-    },
-    toggleChildren() {
-      this.fetchData()
-      this.showChildren = !this.showChildren;
-    },
-    fetchData() {
-      // create a child to stub a loading message
-      this.children = [{id: (new Date().getTime() * -1), name:"Loading...." }]
-      this.$http.get(`/api/v1/folders/${this.node.id}`)
-        .then(
-          response => {
-
-          this.children = response.data.data.children
-          this.dataLoaded = true;
-
-          }
-        )
-
-          // console.log(response.data.data.children)
-        .catch((error) => {
-          console.log(error);
-        });
-
-    }
-  }
-})
-
 document.addEventListener('DOMContentLoaded', () => {
   var app = new Vue({
     el: '#app',
     store,
-    components: { plyr },
+    components: { plyr, TreeNode},
     data: {
       message: 'Hello Vue!',
       treeData: [{id: (new Date().getTime() * -1), name:"Loading...." }]
