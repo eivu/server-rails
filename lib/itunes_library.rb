7738cd74246ce7e9eb2ddd3e480aa7e1f3b4caa8
Robert Jenkins <bobert@gmail.com>
2019-04-05 17:55:52 -0400

    created ItunesTrackInfo and progress with ItunesLibrary

--- lib/itunes_library.rb
+++ lib/itunes_library.rb
@@ -2,9 +2,10 @@ class ItunesLibrary
 
   attr_reader :path, :parser, :tracks
 
-  def initialize(path="")
-    path   = "/Users/jinx/Music/iTunes/iTunes\ Music\ Library.xml" if path.blank?
-    @path  = path
+  def initialize(path="", bucket)
+    path    = "/Users/jinx/Music/iTunes/iTunes\ Music\ Library.xml" if path.blank?
+    @path   = path
+    @bucket = Bucket.determine(bucket)
     @parser = ItunesParser.new(:file => @path)
     @tracks = @parser.tracks.values
     @num_tracks = @tracks.count
@@ -33,13 +34,11 @@ class ItunesLibrary
   end
 
 
-  def ingest
+  def ingest_and_indentify
+    binding.pry
     @tracks.each do |raw_track_info|
-      track_info = raw_track_info.simplify
-      track_info[:total_time] = track_info[:total_time].to_i/1000
-      mash = Hashie::Mash.new track_info
-      binding.pry
-      1
+      track_info = ItunesTrackInfo.new(raw_track_info)
+      cloud_file = CloudFile.ingest(track_info.path_to_file, @bucket, :itunes_track_info => track_info)
     end
   end
 
---
+++ lib/itunes_track_info.rb
@@ -0,0 +1,22 @@
+class ItunesTrackInfo
+
+  def initialize(raw_track_info)
+    @track_info = raw_track_info.simplify
+    @track_info[:total_time] = @track_info[:total_time].to_i/1000
+  end
+
+  def path_to_file
+    @track_info[:location].gsub("file:///", "/")
+  end
+
+  def method_missing(method, *args)
+    method = method.to_sym
+    #sometimes @attr is nil, *shrug*
+    if @track_info[method].present?
+      return @track_info[method]
+    else
+      super
+    end
+  end
+
+end
