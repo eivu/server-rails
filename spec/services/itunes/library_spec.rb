9e17223ef737d43867c1699165102b2efd6b0660
Robert Jenkins <bobert@gmail.com>
2019-08-25 18:31:42 -0400

    added more specs to library_spec

--- spec/services/itunes/library_spec.rb
+++ spec/services/itunes/library_spec.rb
@@ -1,22 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Itunes::Library, type: :service do
-  describe '#new' do
+  describe 'new instance' do
     let(:path_to_file) { "#{Rails.root}/spec/fixtures/files/itunes/library.xml" }
 
     subject { Itunes::Library.new(path_to_file) }
 
-    it 'should have 4 tracks' do
+    specify '#num_tracks to have 4 tracks' do
       expect(subject.num_tracks).to eq(4)
     end
 
-    it 'should return path to library' do
+    specify '#path to return path to library' do
       expect(subject.path).to eq(path_to_file)
     end
 
-    it 'should have an array of Itunes::Track objects' do
+    specify '#tracks to be an array of Itunes::Track objects' do
       subject.tracks.all? do |track|
         expect(track).to be_a(Itunes::Track)
+      end
+    end
+
+    specify '#raw_tracks to be an array of hashes' do
+      subject.raw_tracks.all? do |raw_track|
+        expect(raw_track).to be_a(Hash)
       end
     end
   end
