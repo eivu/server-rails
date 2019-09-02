fa97f10e7ee2e63fc394ee281008f85892779546
Robert Jenkins <bobert@gmail.com>
2019-09-02 19:00:29 -0400

    refacted spec/services/fingerprinter/acoustid/parser_spec.rb with best practices

--- spec/services/fingerprinter/acoustid/parser_spec.rb
+++ spec/services/fingerprinter/acoustid/parser_spec.rb
@@ -7,18 +7,20 @@ RSpec.describe Fingerprinter::Acoustid::Parser, type: :service do
   describe '#parse_artists' do
     describe 'with single artist' do
       subject { described_class.parse_artists([{"id"=>"164f0d73-1234-4e2c-8743-d77bf2191051", "name"=>"Kanye West"}]) }
-      it 'should parse a single artist ' do
+      it 'should succeed ' do
         is_expected.to eq([{ primary: true, name: 'Kanye West', ext_id: '164f0d73-1234-4e2c-8743-d77bf2191051'}])
       end
     end
 
-    it 'should parse 2 primary artists and a secondary' do
-      result = described_class.parse_artists([{"id"=>"f82bcf78-5b69-4622-a5ef-73800768d9ac", "joinphrase"=>" & ", "name"=>"JAY Z"},
+    describe 'with two artists' do
+      subject { described_class.parse_artists([{"id"=>"f82bcf78-5b69-4622-a5ef-73800768d9ac", "joinphrase"=>" & ", "name"=>"JAY Z"},
                    {"id"=>"164f0d73-1234-4e2c-8743-d77bf2191051", "joinphrase"=>" feat. ", "name"=>"Kanye West"},
-                   {"id"=>"859d0860-d480-4efd-970c-c05d5f1776b8", "name"=>"Beyoncé"}])
-      expect(result).to eq([{name: 'JAY Z', primary: true, ext_id: 'f82bcf78-5b69-4622-a5ef-73800768d9ac'},
-                   {name: 'Kanye West', primary: false, ext_id: '164f0d73-1234-4e2c-8743-d77bf2191051'},
-                   {name: 'Beyoncé', primary: false, ext_id: '859d0860-d480-4efd-970c-c05d5f1776b8'}])
+                   {"id"=>"859d0860-d480-4efd-970c-c05d5f1776b8", "name"=>"Beyoncé"}]) }
+      it 'should succeed ' do
+        is_expected.to eq([{name: 'JAY Z', primary: true, ext_id: 'f82bcf78-5b69-4622-a5ef-73800768d9ac'},
+                     {name: 'Kanye West', primary: false, ext_id: '164f0d73-1234-4e2c-8743-d77bf2191051'},
+                     {name: 'Beyoncé', primary: false, ext_id: '859d0860-d480-4efd-970c-c05d5f1776b8'}])
+      end
     end
   end
 end
