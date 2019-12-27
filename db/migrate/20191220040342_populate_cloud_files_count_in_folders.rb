001aed94ef21e1b1187d391fdf970a0e1a3dccd5
Robert Jenkins <bobert@gmail.com>
2019-12-27 11:22:37 -0500

    fixed migration, cloud file counts are now properly set

--- db/migrate/20191220040342_populate_cloud_files_count_in_folders.rb
+++ db/migrate/20191220040342_populate_cloud_files_count_in_folders.rb
@@ -1,7 +1,7 @@
 class PopulateCloudFilesCountInFolders < ActiveRecord::Migration[5.2]
   def up
     Folder.unscoped.all.each do |f|
-      count = f.cloud_files.count
+      count = CloudFile.unscoped.where(folder_id: f.id).count
       Folder.where(id: f.id).update_all(cloud_files_count: count)
       puts "set #{f.name}(#{f.id}) => #{count}"
     end
