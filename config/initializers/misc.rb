0f407aae96eaa1f828e84f19cbec2684d9679389
Robert Jenkins <bobert@gmail.com>
2021-08-01 18:49:02 -0400

    removing file due to security risk

--- config/initializers/misc.rb
+++
@@ -1,2 +0,0 @@
-# this should be moved to aws secrets
-ATTR_ENCRYPTED_SECURITY_KEY = "\v\x8F1\xFC\x8Ae\xC0\u000E\u0013\xC7\xFE\x99\u0005\x90\u0006\u0019}\xE3\xD5R\u0003\xE8\u0010\xF9\u0014q\x86U\t\x8C\x83n"
