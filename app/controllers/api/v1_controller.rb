bed6695985b6b28edc1180c83735b6df19103d8d
Robert Jenkins <bobert@gmail.com>
2021-08-10 15:47:28 -0400

    removed nested compact class declarations

--- app/controllers/api/v1_controller.rb
+++ app/controllers/api/v1_controller.rb
@@ -1,21 +1,24 @@
-class Api::V1Controller < ApplicationController
-  skip_before_action :verify_authenticity_token, :authenticate_user!
-  before_action :authenticate_by_token
+# frozen_string_literal: true
 
-  def info
-    render json: { version: '0.2.3' }
-  end
+module Api
+  class V1Controller < ApplicationController
+    skip_before_action :verify_authenticity_token, :authenticate_user!
+    before_action :authenticate_by_token
 
-  ############################################################################
-  private
-  ############################################################################
+    def info
+      render json: { version: '0.2.3' }
+    end
 
-  def authenticate_by_token
-    authenticate_or_request_with_http_token do |token, options|
-      user = User.find_by_token(token)
-      # Compare the tokens in a time-constant manner, to mitigate
-      # timing attacks.
-      @current_user = user if user && ActiveSupport::SecurityUtils.secure_compare(token, user&.token)
+    ############################################################################
+    private
+    ############################################################################
+
+    def authenticate_by_token
+      authenticate_or_request_with_http_token do |token, options|
+        user = User.find_by_token(token)
+        # Compare the tokens in a time-constant manner, to mitigate timing attacks.
+        @current_user = user if user && ActiveSupport::SecurityUtils.secure_compare(token, user&.token)
+      end
     end
   end
 end
