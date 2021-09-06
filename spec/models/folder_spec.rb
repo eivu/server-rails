3b828668948412949bfa2e11b4e05fa90c5c9e36
Robert Jenkins <bobert@gmail.com>
2021-09-06 12:42:32 -0400

    progress with specs

--- spec/models/folder_spec.rb
+++ spec/models/folder_spec.rb
@@ -44,16 +44,38 @@ RSpec.describe Folder, type: :model do
     context 'doubley nested folder' do
       let(:path) { 'folder_a/folder_b' }
 
-      it 'creates two folders' do
-        expect { instance }.to change(Folder, :count).by(2)
+      context 'folder_a does NOT exist' do
+        it 'creates two folders' do
+          expect { instance }.to change(Folder, :count).by(2)
+        end
+
+        it 'creates a folder named folder_b' do
+          expect(instance.name).to eq('folder_b')
+        end
+
+        it 'has a parent named folder_a' do
+          expect(instance.parent.name).to eq('folder_a')
+        end
       end
 
-      it 'creates a folder named folder_b' do
-        expect(instance.name).to eq('folder_b')
-      end
+      context 'folder_a does exist' do
+        let!(:parent) { Folder.create name: 'folder_a' }
 
-      it 'has a parent named folder_a' do
-        expect(instance.parent.name).to eq('folder_a')
+        it 'creates a single folder' do
+          expect { instance }.to change(Folder, :count).by(1)
+        end
+
+        it 'creates a folder named folder_b' do
+          expect(instance.name).to eq('folder_b')
+        end
+
+        it 'has a parent named folder_a' do
+          expect(instance.parent.name).to eq('folder_a')
+        end
+
+        it 'has a parent that id matches the existing folder' do
+          expect(instance.parent).to eq(parent)
+        end
       end
     end
   end
