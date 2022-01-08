0cec799ba3de7b277f2f813736ce9a1e281bd703
Robert Jenkins <bobert@gmail.com>
2022-01-08 00:28:12 -0400

    actually added storng migrations unlike the last commit that says so

--- db/migrate/20220107231348_replace_attr_encrypted_columns.rb
+++ db/migrate/20220107231348_replace_attr_encrypted_columns.rb
@@ -4,11 +4,11 @@ class ReplaceAttrEncryptedColumns < ActiveRecord::Migration[7.0]
     add_column :users, :access_key_id, :string
     add_column :users, :secret_access_key, :string
     # delete old columns for attr_encrypted
-    remove_column :users, :encrypted_access_key_id
-    remove_column :users, :encrypted_access_key_id_salt
-    remove_column :users, :encrypted_access_key_id_iv
-    remove_column :users, :encrypted_secret_access_key
-    remove_column :users, :encrypted_secret_access_key_salt
-    remove_column :users, :encrypted_secret_access_key_iv
+    remove_column :users, :encrypted_access_key_id, :string
+    remove_column :users, :encrypted_access_key_id_salt, :string
+    remove_column :users, :encrypted_access_key_id_iv, :string
+    remove_column :users, :encrypted_secret_access_key, :string
+    remove_column :users, :encrypted_secret_access_key_salt, :string
+    remove_column :users, :encrypted_secret_access_key_iv, :string
   end
 end
