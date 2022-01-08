4f868ea4c1e779847a3efd9e3cfef2003b58024d
Robert Jenkins <bobert@gmail.com>
2022-01-08 00:28:42 -0400

    commented out line

--- spec/models/cloud_file_spec.rb
+++ spec/models/cloud_file_spec.rb
@@ -28,7 +28,7 @@
 #  num_plays      :integer          default(0), not null
 #
 require 'openssl'
-   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+  #  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 require 'rails_helper'
 
