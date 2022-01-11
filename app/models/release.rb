73b4997975f446169719ecdc7f66973d375b611c
Robert Jenkins <bobert@gmail.com>
2022-01-10 22:59:59 -0500

    change in whitespace

--- app/models/release.rb
+++ app/models/release.rb
@@ -17,11 +17,10 @@
 class Release < ApplicationRecord
   include ConfigureAndSavable
   include Reactable
-  
+
   belongs_to :release_type
 
   has_many :cloud_files, dependent: :destroy, counter_cache: :cloud_files_count
   has_many :artist_releases, dependent: :destroy
   has_many :artists, through: :artist_releases
-
 end
