49600ae7ad3388656462136cef73f3198a3a8b1c
Robert Jenkins <bobert@gmail.com>
2022-01-17 16:05:13 -0500

    hotwire for single folder works

--- app/controllers/overviews_controller.rb
+++ app/controllers/overviews_controller.rb
@@ -1,6 +1,7 @@
 class OverviewsController < ApplicationController
   def show
     @folders = Folder.roots.alpha
+    @folder = Folder.last
     # @folder_roots = Folder.includes(:cloud_files, bucket: [:region]).roots
   end
 end
