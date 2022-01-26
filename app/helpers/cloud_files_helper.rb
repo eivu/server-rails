d3251b15c260259a5ae0b6b5e842b32fb80f0002
Robert Jenkins <bobert@gmail.com>
2022-01-26 01:34:20 -0500

    progress with rendering SOME flv files

--- app/helpers/cloud_files_helper.rb
+++ app/helpers/cloud_files_helper.rb
@@ -1,6 +1,7 @@
 module CloudFilesHelper
 
   def view_content(cloud_file)
-    render partial: "cloud_files/formats/#{cloud_file.media_type}"
+    template_name = cloud_file.content_type.gsub('/', '_')
+    render partial: "cloud_files/formats/#{template_name}"
   end
 end
