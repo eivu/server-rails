45f4de747c3f46662b803612490562b78ace2dc9
Robert Jenkins <bobert@gmail.com>
2022-07-10 15:34:15 -0400

    fixed rendering of audio content

--- app/helpers/cloud_files_helper.rb
+++ app/helpers/cloud_files_helper.rb
@@ -1,7 +1,11 @@
 module CloudFilesHelper
 
   def view_content(cloud_file)
-    template_name = cloud_file.content_type.gsub('/', '_')
-    render partial: "cloud_files/formats/#{template_name}"
+    if cloud_file.content_type&.include?('audio')
+      render partial: "cloud_files/formats/audio"
+    else
+      template_name = cloud_file.content_type.gsub('/', '_')
+      render partial: "cloud_files/formats/#{template_name}"
+    end
   end
 end
